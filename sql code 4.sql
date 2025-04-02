40a4c7056f9688ceedca543184884cd517983339
Yuvansh Raj Saxena <yuvansh07raj@gmail.com>
2025-04-02 22:41:10 +0530

    Update sql code 4.sql

--- sql code 4.sql
+++ sql code 4.sql
@@ -1,25 +1,34 @@
-CREATE DATABASE org;
+CREATE DATABASE org_data;
 
-SHOW DATABASES;
+USE org_data;
 
-USE org123;
-
-CREATE TABLE Persons (
-    ID INT NOT NULL,
-    LastName VARCHAR(255) NOT NULL,
-    FirstName VARCHAR(255) NOT NULL,
-    Age INT
+CREATE TABLE categories (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(30) NOT NULL UNIQUE,
+    description TEXT NOT NULL
 );
 
-DESC Persons;
+INSERT INTO categories VALUES (201, 'Home Decor', 'Contains all decorative wooden items');
 
-ALTER TABLE Persons MODIFY Age INT NOT NULL;
+SELECT * FROM categories;
+DESC categories;
 
-CREATE TABLE Persons1 (
-    ID INT PRIMARY KEY,
-    LastName VARCHAR(255) NOT NULL UNIQUE,
-    FirstName VARCHAR(255) NOT NULL UNIQUE,
-    Age INT
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    category_id INT,
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
 
-DESC Persons1;
+DESC products;
+
+DROP TABLE products;
+
+INSERT INTO products VALUES (501, 'Teak Wood Table', NULL);
+
+SELECT * FROM products;
+SELECT * FROM categories;
+
+DELETE FROM categories WHERE category_id = 200;
+
+DROP TABLE categories;
