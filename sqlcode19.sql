a3d8449da02e784b57d48edcfff269eda6790a5f
Yuvansh Raj Saxena <yuvansh07raj@gmail.com>
2025-04-02 23:13:32 +0530

    Create sqlcode19.sql

---
+++ sqlcode19.sql
@@ -0,0 +1,33 @@
+-- Display available databases
+SHOW DATABASES;
+
+-- Sample output:
+-- system_db              40.00 KiB
+-- settings               48.00 KiB
+-- user_data              96.00 KiB
+-- school_management     384.00 KiB
+-- test_db                 8.00 KiB
+-- vit_university         40.00 KiB
+
+-- Switch to the 'settings' database
+USE settings;
+
+-- Confirmation message:
+-- Switched to database 'settings'
+
+-- Switch to the 'vit_university' database
+USE vit_university;
+
+-- Confirmation message:
+-- Switched to database 'vit_university'
+
+-- Show databases again
+SHOW DATABASES;
+
+-- Sample output:
+-- system_db              40.00 KiB
+-- settings               48.00 KiB
+-- user_data              96.00 KiB
+-- school_management     384.00 KiB
+-- test_db                 8.00 KiB
+-- vit_university         40.00 KiB
